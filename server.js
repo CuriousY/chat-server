31d83f6a3f6bbaa13390e3e520748be92998ed0d
Nitesh <nickscorpio4u@gmail.com>
2018-05-05 11:11:08 +0530

    server changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/writeJSON', (req, res) => {
 
 app.post('/postImage', upload.array(), function (req, res, next) {
   // req.body contains the text fields
-  console.log('inside post ', req.body);
+  console.log('inside post ', req);
   res.send({success:'true'});
 });
 
